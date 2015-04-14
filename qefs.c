4e69c1d9882e8564e65fe343c8b279549650fe69
forth32 <forth32@mail.ru>
2015-04-14 12:38:44 +0300

    qefs: добавлено чтение записей EFS

--- qefs.c
+++ qefs.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
 #ifndef WIN32
 #include <unistd.h>
 #include <getopt.h>
@@ -10,6 +11,14 @@
 #include "printf.h"
 #endif
 #include "qcio.h"
+
+//%%%%%%%%%  Общие переменные %%%%%%%%%%%%%%%%55
+
+unsigned int altflag=0;   // флаг альтернативной EFS
+unsigned int fixname=0;   // индикатор явного уазания имени файла
+unsigned int zeroflag=0;  // флаг пропуска пустых записей nvram
+char filename[50];        // имя выходного файла
+
 
 //****************************************************
 //* Проверка массива на ненулевые значения
@@ -25,42 +34,136 @@ for (i=0;i<len;i++)
 return 0;
 }
 
-//*******************************************
-//@@@@@@@@@@@@ Головная программа
-void main(int argc, char* argv[]) {
-  
+
+//****************************************************
+//* Чтение дампа EFS (efs.mbn)
+//****************************************************
+
+void back_efs() {
+
 unsigned char iobuf[14048];
 unsigned char cmd_efsh1[16]=  {0x4B, 0x13,0x19, 0};
 unsigned char cmd_efsopen[16]=  {0x4B, 0x13,0x16, 0};
 unsigned char cmd_efsdata[16]={0x4B, 0x13,0x17,0,0,0,0,0,0,0,0,0};
 unsigned char cmd_efsclose[16]=  {0x4B, 0x13,0x18, 0};
 
-unsigned int altflag=0;
-unsigned int fixname=0;
-int iolen,opt,i;
+int iolen,i;
 FILE* out;
+
+// настройка на альтернативную EFS
+if (altflag) {
+ cmd_efsh1[1]=0x3e;
+ cmd_efsopen[1]=0x3e;
+ cmd_efsdata[1]=0x3e;
+ cmd_efsclose[1]=0x3e;
+ if (!fixname) strcpy(filename,"efs_alt.mbn");
+}
+// настройка на стандартную EFS
+else strcpy(filename,"efs.mbn");
+   
+out=fopen(filename,"w");
+  
+iolen=send_cmd_base(cmd_efsh1, 4, iobuf, 0);
+if ((iolen != 11) || test_zero(iobuf+3,5)) {
+  printf("\n Неправильный ответ на команду 19\n");
+  dump(iobuf,iolen,0);
+  return;
+}
+
+iolen=send_cmd_base(cmd_efsopen, 4, iobuf, 0);
+if ((iolen != 11) || test_zero(iobuf+3,5)) {
+  printf("\n Неправильный ответ на команду открытия (16)\n");
+  dump(iobuf,iolen,0);
+  return;
+}
+printf("\n");
+
+// главный цикл получения efs.mbn
+while(1) {
+  iolen=send_cmd_base(cmd_efsdata, 12, iobuf, 0);
+  if (iolen != 532) {
+    printf("\n Неправильный ответ на команду 4b 13 17\n");
+    dump(iobuf,iolen,0);
+    return;
+  }
+  printf("\r Читаем раздел: %i   блок: %08x",*((unsigned int*)&iobuf[8]),*((unsigned int*)&iobuf[12]));
+  fflush(stdout);
+  if (iobuf[8] == 0) break;
+  fwrite(iobuf+16,512,1,out);
+  memcpy(cmd_efsdata+4,iobuf+8,8);
+}
+// закрываем EFS
+iolen=send_cmd_base(cmd_efsclose,4,iobuf,0);
+if ((iolen != 11) || test_zero(iobuf+3,5)) {
+  printf("\n Неправильный ответ на команду закрытия (18)\n");
+  dump(iobuf,iolen,0);
+}
+}
+
+
+
+//*******************************************
+//*  Чтение всех заисей NVRAM
+//*******************************************
+void read_nvram_items() {
+  
+unsigned char iobuf[14048];
+unsigned char cmd_rd[16]=  {0x26, 0,0};
+
+unsigned int nv,iolen;
+char filename[50];
+FILE* out;
+
+// Создаем каталог nv для сбора файлов
+if (mkdir("nv",0777) != 0) 
+  if (errno != EEXIST) {
+    printf("\n Невозможно создать каталог nv/");
+    return;
+  }  
+
+printf("\n");  
+for(nv=0;nv<0x10000;nv++) {
+  printf("\r NV %04x",nv); fflush(stdout);
+  *((unsigned short*)&cmd_rd[1])=nv;
+  iolen=send_cmd_base(cmd_rd,3,iobuf,0);
+  if (iolen != 136) continue;
+  if (zeroflag && (test_zero(iobuf+3,128) == 0)) continue;
+  sprintf(filename,"nv/%04x-%04x",nv,*((unsigned short*)&iobuf[130]));
+  out=fopen(filename,"w");
+  fwrite(iobuf+3,1,128,out);
+  fclose(out);
+}  
+}  
+  
+//@@@@@@@@@@@@ Головная программа
+void main(int argc, char* argv[]) {
+
+unsigned int opt;
+  
 enum{
   MODE_BACK_EFS,
-  MODE_BACK_NVRAM
+  MODE_BACK_NVRAM,
+  MODE_READ_NVRAM
 }; 
 
 int mode=-1;
-
-char filename[50]="efs.mbn";
 
 #ifndef WIN32
 char devname[50]="/dev/ttyUSB0";
 #else
 char devname[50]="";
 #endif
-while ((opt = getopt(argc, argv, "po:ab")) != -1) {
+
+while ((opt = getopt(argc, argv, "hpo:ab:r:")) != -1) {
   switch (opt) {
    case 'h': 
     printf("\n  Утилита предназначена для работы с разделом efs \n\
 Допустимы следующие ключи:\n\n\
 Ключи, определяюще выполняемую операцию:\n\
 -be       - дамп efs\n\
--bn       - дамп nvram\n\n\
+-bn       - дамп nvram\n\
+-rn[z]    - чтение всех записей nvram в отдельные файлы (z-пропускать пустые записи)\n\
+\n\
 Ключи-модификаторы:\n\
 -p <tty>  - указывает имя устройства диагностического порта модема\n\
 -a        - использовать альтернативную EFS\n\
@@ -72,7 +175,7 @@ while ((opt = getopt(argc, argv, "po:ab")) != -1) {
      strcpy(filename,optarg);
      fixname=1;
      break;
-
+   //  === группа ключей backup ==
    case 'b':
      if (mode != -1) {
        printf("\n В командной строке задано более 1 ключа режима работы");
@@ -89,6 +192,24 @@ while ((opt = getopt(argc, argv, "po:ab")) != -1) {
 	 
        default:
 	 printf("\n Неправильно задано значение ключа -b\n");
+	 return;
+      }
+      break;
+
+   //  === группа ключей read ==
+   case 'r':
+     if (mode != -1) {
+       printf("\n В командной строке задано более 1 ключа режима работы");
+       return;
+     }  
+     switch(*optarg) {
+       case 'n':
+         mode=MODE_READ_NVRAM;
+	 if (optarg[1] == 'z') zeroflag=1;
+         break;
+	 
+       default:
+	 printf("\n Неправильно задано значение ключа -r\n");
 	 return;
       }
       break;
@@ -130,54 +251,22 @@ if (!open_port(devname))  {
 }
 
 
+//////////////////
 
-// настройка на альтернативную EFS
-if (altflag) {
- cmd_efsh1[1]=0x3e;
- cmd_efsopen[1]=0x3e;
- cmd_efsdata[1]=0x3e;
- cmd_efsclose[1]=0x3e;
- if (!fixname) strcpy(filename,"efs_alt.mbn");
-}   
-   
-out=fopen(filename,"w");
-  
-iolen=send_cmd_base(cmd_efsh1, 4, iobuf, 0);
-if ((iolen != 11) || test_zero(iobuf+3,5)) {
-  printf("\n Неправильный ответ на команду 19\n");
-  dump(iobuf,iolen,0);
-  return;
-}
-
-iolen=send_cmd_base(cmd_efsopen, 4, iobuf, 0);
-if ((iolen != 11) || test_zero(iobuf+3,5)) {
-  printf("\n Неправильный ответ на команду открытия (16)\n");
-  dump(iobuf,iolen,0);
-  return;
-}
-printf("\n");
-
-// главный цикл получения efs.mbn
-while(1) {
-  iolen=send_cmd_base(cmd_efsdata, 12, iobuf, 0);
-  if (iolen != 532) {
-    printf("\n Неправильный ответ на команду 4b 13 17\n");
-    dump(iobuf,iolen,0);
+switch (mode) {
+  case MODE_BACK_EFS:
+    back_efs();
+    break;
+    
+  case MODE_READ_NVRAM:
+    read_nvram_items();
+    break;
+    
+  default:
+    printf("\n Не указан ключ выполняемой операции\n");
     return;
-  }
-  printf("\r Читаем раздел: %i   блок: %08x",*((unsigned int*)&iobuf[8]),*((unsigned int*)&iobuf[12]));
-  fflush(stdout);
-  if (iobuf[8] == 0) break;
-  fwrite(iobuf+16,512,1,out);
-  memcpy(cmd_efsdata+4,iobuf+8,8);
-}
-// закрываем EFS
-iolen=send_cmd_base(cmd_efsclose,4,iobuf,0);
-if ((iolen != 11) || test_zero(iobuf+3,5)) {
-  printf("\n Неправильный ответ на команду закрытия (18)\n");
-  dump(iobuf,iolen,0);
-  return;
-}
+
+}    
 printf("\n");
 
 }
