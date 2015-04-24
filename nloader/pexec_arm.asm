fa995911cb013ebababd8202c2f65c6dc557f171
forth32 <forth32@mail.ru>
2015-04-24 16:59:19 +0300

    Исхдник ARM-версии патча

---
+++ nloader/pexec_arm.asm
@@ -0,0 +1,23 @@
+.syntax unified 
+
+pkt_data_len_off=10
+		.ORG		0x0011607c
+leavecmd:
+
+		.ORG		0x00116110
+cmd_11_exec:
+		PUSH		{R4,LR}
+		LDR		R1, cmd_reply_code_ptr 	     @ Адрес ответнго буфера
+		ADD		R0, R0,	#8	             @ R0 теперь показывает на байт +4 от начала
+							     @ командного буфера
+		MCR 		p15, 0, R1,c7,c5,0           @ сброс I-кеша
+		BLX		R0               	     @ передаем туда управление
+		LDR		R0, cmd_processor_data_ptr   @  Структура данных командного обработчика
+		STRH		R4, [R0,#pkt_data_len_off]   @ сохраняем размер ответного буфера
+		B		leavecmd
+
+		.ORG  0x00116140
+		
+cmd_reply_code_ptr:      .word  0
+cmd_processor_data_ptr:  .word  0
+
