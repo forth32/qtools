5e120a3091924e64c96ca816f8eb8d93a424d523
forth32 <you@example.com>
2016-01-13 15:42:05 +0300

    Подправле текст патча загрузчиков в коде ARM

--- loaders/pexec_arm.asm
+++ loaders/pexec_arm.asm
@@ -3,26 +3,26 @@
 @
 .syntax unified 
 
-pkt_data_len_off=10
-		.ORG		0x0011607c
+pkt_data_len_off=8
+		.ORG		0x00127140
 leavecmd:
 
-		.ORG		0x00116110
+		.ORG		0x00127158
 cmd_11_exec:
 		PUSH		{R4,LR}
-		LDR		R1, cmd_reply_code_ptr 	     @ Адрес ответнго буфера
+		LDR		R1, reply_buf_ptr 	     @ Адрес ответнго буфера
 		ADD		R0, R0,	#8	             @ R0 теперь показывает на байт +4 от начала
 							     @ командного буфера
 		MCR 		p15, 0, R1,c7,c5,0           @ сброс I-кеша
 		BLX		R0               	     @ передаем туда управление
-		LDR		R0, cmd_processor_data_ptr   @  Структура данных командного обработчика
+		LDR		R0, escape_state_ptr         @  Структура данных командного обработчика
 		STRH		R4, [R0,#pkt_data_len_off]   @ сохраняем размер ответного буфера
 		B		leavecmd
 @ блок идентификации
                 .word 		0xdeadbeef   @ сигнатура
-                .byte           3            @ код чипсета
+                .byte           2            @ код чипсета
 
-		.ORG  0x00116140
+		.ORG  0x00127188
 		
 reply_buf_ptr:    .word  0
 escape_state_ptr: .word  0
